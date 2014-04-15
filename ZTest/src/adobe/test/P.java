51c05f90813bc8b37a5a02ebaf95a5e96ae449d4
Prateek Rathore <appcrazyprateek@gmail.com>
2014-04-15 09:16:14 +0530

    ASd

--- ZTest/src/adobe/test/P.java
+++ ZTest/src/adobe/test/P.java
@@ -3,20 +3,20 @@ package adobe.test;
 import java.util.*;
 class P{
  public static void main(String[]a){
-		long N=5,c=0,S=0,k=7,p,d,s,n; 
+		long N=5,c=0,S=0,n=7,d,s,k;
 		
 		while(c<N){
-			n=k;
+			k=n;
 			//d=1;
 			//  while(++d<n)if(n%d<1)p=0;
-			  for(Set h=new HashSet();h.add(n);n=s)
-				  for(s=0;n>0;s+=d*d,n/=10)d=n%10;	
+			  for(Set h=new HashSet();h.add(k);k=s)
+				  for(s=0;k>0;s+=d*d,k/=10)d=k%10;	
 			  
-			  if(n<2){
-				  System.out.println(k);
-				  c++;S+=k;}
-			k+=2;
+			  if(k<2){c++;S+=n;
+				  System.out.println(n);
+				  }
+			n+=2;
 		}
-		System.out.print(S);
+		//System.out.print(S);
 	}
 }
