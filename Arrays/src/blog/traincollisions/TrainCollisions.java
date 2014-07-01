d2924d94ad8c5630ab5217c31351f598af068f2c
Prateek Rathore <appcrazyprateek@gmail.com>
2014-07-01 09:35:40 +0530

    Train Collision

---
+++ Arrays/src/blog/traincollisions/TrainCollisions.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package blog.traincollisions;
+
+import java.util.List;
+
+
+class Interval
+{
+	public int start;
+	public int end;
+	
+	public Interval() {
+		start=0;
+		end =0;
+	}
+	
+	public Interval(int start,int end){
+		this.start=start;
+		this.end=end;
+	}
+
+	@Override
+	public String toString() {
+		return this.start +" --> "+this.end;
+	}
+}
+/**
+ * @author PRATEEK
+ *
+ */
+public class TrainCollisions {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public int stations(List<Interval> time){
+		
+		
+		
+		
+		return 0;
+		
+	}
+}
